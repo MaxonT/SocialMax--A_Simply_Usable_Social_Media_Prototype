3ea71ff9bddc5ca101d31ff5803f987e70112024
TigerYangMing <mexotiger@gmail.com>
2025-09-06 14:43:21 -0500

    Make

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'      // Introduce your application's main component.
+import App from './App.jsx'      
 import './index.css'             
 
-//Find the `<div id="root">` in `index.html` and render `<App />` inside it.
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
