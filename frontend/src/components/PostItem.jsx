49dd4e37c0737ddeb3c8e393748288ddbde0aef5
TigerYangMing <mexotiger@gmail.com>
2025-09-09 03:22:11 +0800

    Update PostItem.jsx

--- frontend/src/components/PostItem.jsx
+++ frontend/src/components/PostItem.jsx
@@ -14,7 +14,7 @@ export default function PostItem({ post, onLike, onUnlike, onAddComment }) {
         </div>
       </header>
 
-      <p className="post-content">{post.text}</p>
+      <p className="post-content">{post.content}</p>
 
       <div className="row">
         <button onClick={onLike}>👍 Like ({post.likes})</button>
